3691da364b5a82969307b58219875d397b17b60e
sphinx <k.komandin@gmail.com>
2024-06-14 08:16:42 +0500

    add linter config

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -19,44 +19,34 @@ func Unpack(src string) (string, error) {
 	for i, ch := range src {
 		switch {
 		case IsDigit(ch):
-			{
-				if prevNumber || i == 0 {
-					return "", ErrInvalidString
-				}
 
-				if !prevSlash {
-					prevNumber = true
-				}
-				n, _ := strconv.Atoi(string(ch))
-				if n < 0 {
-					return "", ErrInvalidString
-				}
-				switch {
-				case n == 0:
-					{
-						dst = dst[:len(dst)-1]
-					}
-				case prevSlash:
-					{
-						prevNumber = false
-						prevSlash = false
-						dst += string(ch)
-					}
-				default:
-					{
-						dst += strings.Repeat(string(src[i-1]), n-1)
-					}
-				}
+			if prevNumber || i == 0 {
+				return "", ErrInvalidString
 			}
-		case prevSlash && ch == '\\':
-			{
-				dst += string(ch)
+
+			if !prevSlash {
+				prevNumber = true
+			}
+			n, _ := strconv.Atoi(string(ch))
+			if n < 0 {
+				return "", ErrInvalidString
+			}
+			switch {
+			case n == 0:
+				dst = dst[:len(dst)-1]
+			case prevSlash:
+				prevNumber = false
 				prevSlash = false
+				dst += string(ch)
+			default:
+				dst += strings.Repeat(string(src[i-1]), n-1)
 			}
+
+		case prevSlash && ch == '\\':
+			dst += string(ch)
+			prevSlash = false
 		case ch == '\\':
-			{
-				prevSlash = true
-			}
+			prevSlash = true
 		default:
 			dst += string(ch)
 			prevNumber = false
