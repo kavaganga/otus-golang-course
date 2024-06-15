918532a955ec1a898a62247ad5a51082fefc3c80
sphinx <k.komandin@gmail.com>
2024-06-15 14:15:30 +0500

    base case

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(src string) []string {
+	var result []string
+	data := make(map[string]int)
+	arr := strings.Split(src, " ")
+	for _, w := range arr {
+		if w == "" {
+			continue
+		}
+		data[w]++
+	}
+	for k := range data {
+		result = append(result, k)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		w1 := result[i]
+		w2 := result[j]
+		switch {
+		case data[w1] > data[w2]:
+			return true
+		case data[w1] == data[w2]:
+			return w1 > w2
+		default:
+			return false
+		}
+	})
+	if len(result) < 10 {
+		return result
+	}
+	return result[:10]
 }
