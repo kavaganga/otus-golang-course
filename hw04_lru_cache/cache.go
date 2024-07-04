ba30102e63584e8e51b1bbbacc11c301619febde
sphinx <k.komandin@gmail.com>
2024-07-04 06:28:45 +0500

    Добавил использование мьютекса в кэш

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,8 +11,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mu       sync.RWMutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -21,5 +22,44 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, ok := c.items[key]
+	if ok {
+		c.items[key].Value = value
+		c.queue.MoveToFront(c.items[key])
+	} else {
+		if c.queue.Len() == c.capacity {
+			c.queue.Remove(c.queue.Back())
+		}
+		c.items[key] = c.queue.PushFront(value)
+	}
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	item, ok := c.items[key]
+	if !ok {
+		return nil, ok
+	}
+	c.queue.MoveToFront(item)
+	return item.Value, true
+}
+
+func (c *lruCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for k, v := range c.items {
+		c.queue.Remove(v)
+		delete(c.items, k)
 	}
 }
